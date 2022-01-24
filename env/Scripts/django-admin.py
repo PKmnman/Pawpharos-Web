8a0183938444788ecfe3dd18436f0d2f8fb3bd6e
Gary Reeves <pkmnman@outlook.com>
2022-01-24 18:42:10 -0500

    Removed virtual environment from repository

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\dev\WebProjects\PetBeaconWebsite\PetBeaconWebsite\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
