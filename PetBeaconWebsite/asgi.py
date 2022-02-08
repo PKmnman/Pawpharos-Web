d6c75cec16edf2aab73269e1eb24f15ee07bc8fc
Gary Reeves <pkmnman@outlook.com>
2022-02-08 15:48:13 -0500

    Temporarily disabled channels

--- PetBeaconWebsite/asgi.py
+++ PetBeaconWebsite/asgi.py
@@ -5,6 +5,4 @@ from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PetBeaconWebsite.settings')
 
-application = ProtocolTypeRouter({
-	"http": get_asgi_application(),
-})
+application = get_asgi_application()
