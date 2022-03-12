e1935fed2faca7eb5051ae084e30e9a2e90a5db7
Gary Reeves <gareeves@my.bridgeport.edu>
2022-03-12 11:22:41 -0500

    Seperated production settings from dev settings

--- PetBeaconWebsite/settings.py
+++ PetBeaconWebsite/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'ccd6c2d8-6ab1-45ad-9565-a452f5001760'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'pawpharos.com', 'www.pawpharos.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 APPEND_SLASH = False
 
@@ -68,31 +68,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'PetBeaconWebsite.wsgi.application'
-ASGI_APPLICATION = 'PetBeaconWebsite.asgi.application'
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DEV_DB = os.getenv('DEV_MODE') is not None
-
-if DEV_DB:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('POSTGRES_NAME'),
-            'USER': os.environ.get('POSTGRES_USER'),
-            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-            'HOST': 'db',
-            'PORT': 5432,
-        }
-    }
+#ASGI_APPLICATION = 'PetBeaconWebsite.asgi.application'
 
 
 LOGIN_URL = '/login/'
@@ -158,20 +134,10 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-AWS_S3_CUSTOM_DOMAIN = 'static.pawpharos.com'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'pawpharos-static'
-AWS_DEFAULT_ACL = 'public-read'
-AWS_S3_ENDPOINT_URL = 'https://nyc3.digitaloceanspaces.com'
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
 
-AWS_LOCATION = 'static'
-STATIC_URL = 'https://nyc3.digitaloceanspaces.com/'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'static/'
 
