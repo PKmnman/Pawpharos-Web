1dba105bddcc38341898834eaa571e8ea17c160f
Gary Reeves <gareeves@my.bridgeport.edu>
2022-03-16 18:16:53 -0400

    Added models to admin page

--- app/admin.py
+++ app/admin.py
@@ -4,3 +4,12 @@ from app import models
 @admin.register(models.Sniffer)
 class SnifferAdmin(admin.ModelAdmin):
 	pass
+
+@admin.register(models.BeaconDevice)
+class BeaconAdmin(admin.ModelAdmin):
+	pass
+
+
+@admin.register(models.TrackingEvent)
+class EventAdmin(admin.ModelAdmin):
+	ordering = ['time']
--- app/models.py
+++ app/models.py
@@ -11,21 +11,21 @@ from uuid import uuid4
 
 # Model for storing the uuid of beacon devices
 class BeaconDevice(models.Model):
-
-    class ModelName(models.TextChoices):
-        DEV_MODEL = 'DEV', 'Development Model'
-        IBEACON = 'IB', 'iBeacon' # For future iBeacon support
-
     device_name = models.TextField(max_length=64, default="Beacon Device", null=True)
 
     # This is the UUID the beacon broadcasts
     bc_uuid = models.UUIDField("Broadcast UUID", null=False, unique=True)
+    # Whether or not the beacon is being actively tracked
+    is_active = models.BooleanField(default=False)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='beacons')
+
+    class Meta:
+        verbose_name = "Beacon Device"
 
 
 # Stores data about registered sniffers
 class Sniffer(models.Model):
-    device_name = models.TextField(max_length=64)
+    device_name = models.CharField(max_length=125)
     serial_code = models.SlugField(max_length=19)
     is_master = models.BooleanField(default=False)
     owner = models.ForeignKey(
@@ -43,10 +43,11 @@ class Location(models.Model):
     account = models.ForeignKey(User, on_delete=models.CASCADE, related_name="locations")
 
 
+
 # Stores data for pets
 class Pet(models.Model):
-    name = models.TextField()
-    species = models.TextField()
+    name = models.CharField(max_length=125)
+    species = models.CharField(max_length=64)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='pets')
     beacon = models.OneToOneField(
         BeaconDevice, 
@@ -62,7 +63,9 @@ class TrackingEvent(models.Model):
     beacon = models.ForeignKey(BeaconDevice, on_delete=models.CASCADE, related_name="events")
     # Sniffer ID - The sniffer that detected it (1:N)
     sniffer = models.ForeignKey(Sniffer, on_delete=models.CASCADE, related_name="events")
-
     location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="+")
+
+    class Meta:
+        verbose_name = "Tracking Event"
     
     
