64064d652eb07a82a38afd537571cf8fe7ab0ebd
Gary Reeves <gareeves@my.bridgeport.edu>
2022-03-23 10:53:19 -0400

    Fixed models bug

--- app/migrations/0002_beacondevice_is_active_alter_pet_name_and_more.py
+++ app/migrations/0002_beacondevice_is_active_alter_pet_name_and_more.py
@@ -10,11 +10,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='beacondevice',
-            name='is_active',
-            field=models.BooleanField(default=False),
-        ),
         migrations.AlterField(
             model_name='pet',
             name='name',
