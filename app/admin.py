079429224a39d27482e957f3931b872aeaadebaa
Gary Reeves <pkmnman@outlook.com>
2022-04-13 19:27:50 -0400

    Fixed registration and updated admin view

--- app/admin.py
+++ app/admin.py
@@ -5,17 +5,22 @@ from app import models
 
 @admin.register(models.Sniffer)
 class SnifferAdmin(admin.ModelAdmin):
-	fields = ('device_name', 'serial_code', ('owner', 'is_master'))
-	list_display = ('device_name', 'serial_code')
-	list_display_links = ('device_name',)
+	fields = ('location', 'serial_code', ('owner',))
+	list_display = ('serial_code', 'location')
+	list_display_links = ('serial_code',)
 
 
 @admin.register(models.BeaconDevice)
 class BeaconAdmin(admin.ModelAdmin):
-	pass
+	fields = ('mac_addr', 'owner')
+	list_display = ('mac_addr', 'owner')
+	list_display_links = ('mac_addr',)
+	search_fields = ['owner__email', 'mac_addr']
 
 
 @admin.register(models.TrackingEvent)
 class EventAdmin(admin.ModelAdmin):
 	ordering = ['event_time']
+	date_hierarchy = 'event_time'
+	list_display = ('event_time', 'beacon_addr', 'sniffer_serial')
 	list_display_links = ('event_time',)
